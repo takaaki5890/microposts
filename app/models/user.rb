5fb37953bb2113ea8c98ef7f95fa5fd3634591cd
takaaki5890 <taaaaakaaki.0307@gmail.com>
2016-10-27 12:39:05 +0000

    add followings/followers

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ActiveRecord::Base
   before_save { self.email = self.email.downcase }
   
   validates :name, presence: true, length: { maximum: 50 }
-  validates :area, presence: true, length: { maximum: 20 },on :update
+  validates :area, presence: true, length: { maximum: 20 },on: :update
 
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   
@@ -14,4 +14,29 @@ class User < ActiveRecord::Base
   has_secure_password
   
   has_many :microposts
+  
+  has_many :following_relationships, class_name:  "Relationship",
+                                     foreign_key: "follower_id",
+                                     dependent:   :destroy
+  has_many :following_users, through: :following_relationships, source: :followed
+  
+  has_many :follower_relationships, class_name:  "Relationship",
+                                    foreign_key: "followed_id",
+                                    dependent:   :destroy
+  has_many :follower_users, through: :follower_relationships, source: :follower
+  
+  def follow(other_user)
+   following_relationships.find_or_create_by(followed_id: other_user.id)
+  end
+
+  # フォローしているユーザーをアンフォローする
+  def unfollow(other_user)
+    following_relationship = following_relationships.find_by(followed_id: other_user.id)
+    following_relationship.destroy if following_relationship
+  end
+
+  # あるユーザーをフォローしているかどうか？
+  def following?(other_user)
+    following_users.include?(other_user)
+  end
 end
